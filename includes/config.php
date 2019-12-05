3a73b08d1c06c2253dcb7324b8c2d573c4696d55
Jack Veney <jackveney03@gmail.com>
2019-12-05 17:28:43 -0500

    edit config.php

--- includes/config.php
+++ includes/config.php
@@ -18,7 +18,7 @@
 |--------------------------------------------------------------------------
 | Author: Jack Veney - Troop 61
 | Project Name: Troop 61 Tent Database
-| Version: v1.4.0
+| Version: v1.5.0
 | Troop Website: https://www.troop61killbuck.org/
 */
   define( 'DB_HOST', 'host' );              // Set database host where it says 'host'
