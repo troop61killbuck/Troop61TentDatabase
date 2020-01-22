b781cd013a7dad3fbb19886f50554d1a99820b8d
Jack Veney <46730283+jackveney@users.noreply.github.com>
2020-01-22 17:53:58 -0500

    Update tent_print_list.php

--- Troop61TentDatabase/tent_print_list.php
+++ Troop61TentDatabase/tent_print_list.php
@@ -18,7 +18,7 @@
     <div class="col-md-12">
       <div class="panel panel-default">
 	  <div class="panel-heading clearfix">
-		<center><h3>Troop 61 Patrol Tent Report</h3><br>Beginning Of Campout List<br><br>Campout:_________________________________</center>
+		<center><h3>Troop 61 Patrol Tent Report</h3><br>Campout:_________________________________</center>
 	  </div>
         <div id="printTable" class="panel-body">
           <table class="table table-bordered" style="width:100%">
@@ -26,8 +26,9 @@
               <tr>
                 <th class="text-center" style="width: 10%;">Tent Number</th>
                 <th class="text-center" style="width: 10%;">Assigned to Patrol</th>
-                <th class="text-center" style="width: 40%;">Scouts In Tent</th>
-                <th class="text-center" style="width: 40%;">Issues With Tent</th>
+                <th class="text-center" style="width: 30%;">Scouts In Tent<br>(Circle Person Taking Home)</th>
+                <th class="text-center" style="width: 30%;">Issues With Tent</th>
+		<th class="text-center" style="width: 20%;">Date Returned</th>
               </tr>
             </thead>
             <tbody>
@@ -35,6 +36,7 @@
               <tr>
                 <td class="text-center"><?php echo remove_junk($tent['tent_number']); ?></td>
                 <td class="text-center"> <?php echo remove_junk($tent['assigned_to_patrol']); ?></td>
+                <td class="text-center"></td>
                 <td class="text-center"></td>
                 <td class="text-center"></td>
               </tr>
@@ -45,39 +47,5 @@
         </div>
       </div>
     </div>
-<p style="page-break-before: always">
- <div class="row">
-     <div class="col-md-12">
-       <?php echo display_msg($msg); ?>
-     </div>
-    <div class="col-md-12">
-      <div class="panel panel-default">
-	  <div class="panel-heading clearfix">
-		<center><h3>Troop 61 Patrol Tent Report</h3><br>After Campout List<br><br>Campout:_________________________________</center>
-	  </div>
-        <div id="printTable" class="panel-body">
-          <table class="table table-bordered" style="width:100%">
-            <thead>
-              <tr>
-                <th class="text-center" style="width: 25%;">Tent Number</th>
-                <th class="text-center" style="width: 25%;">Assigned to Patrol</th>
-                <th class="text-center" style="width: 25%;">Taken Home By</th>
-                <th class="text-center" style="width: 25%;">Date Returned</th>
-              </tr>
-            </thead>
-            <tbody>
-              <?php foreach ($tents as $tent):?>
-              <tr>
-                <td class="text-center"><?php echo remove_junk($tent['tent_number']); ?></td>
-                <td class="text-center"> <?php echo remove_junk($tent['assigned_to_patrol']); ?></td>
-                <td class="text-center"></td>
-                <td class="text-center"></td>
-              </tr>
-             <?php endforeach; ?>
-            </tbody>
-          </table>
-        </div>
-      </div>
-    </div>
-  </div>
+
   <?php include_once('layouts/footer.php'); ?>
