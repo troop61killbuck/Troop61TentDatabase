1eba363ad4a4dbb5cfb470ab6dcc5744aafcd461
Jack Veney <46730283+jackveney@users.noreply.github.com>
2020-01-27 21:22:37 -0500

    Update functions.js

--- libs/js/functions.js
+++ libs/js/functions.js
@@ -1,9 +1,16 @@
+  $(document).ready(function() {
 
+    //tooltip
+    $('[data-toggle="tooltip"]').tooltip();
 
     $('.submenu-toggle').click(function () {
        $(this).parent().children('ul.submenu').toggle(200);
     });
- 
+    //suggetion for finding product names
+    suggetion();
+    // Callculate total ammont
+    total();
+
     $('.datepicker')
         .datepicker({
             format: 'yyyy-mm-dd',
